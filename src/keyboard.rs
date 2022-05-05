ae6b9c00ac676f6b45a92f15d26c80cb74b9a721
JadonBelair <46850171+JadonBelair@users.noreply.github.com>
2022-05-05 17:56:26 -0400

    changed how program checks for latest key press

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -40,25 +40,16 @@ impl Keyboard {
 
     pub fn just_pressed(&self) -> Option<u8> {
         return if let Some(key) = get_last_key_pressed() {
-            match key {
-                KeyCode::X    => Some(0),
-                KeyCode::Key1 => Some(1),
-                KeyCode::Key2 => Some(2),
-                KeyCode::Key3 => Some(3),
-                KeyCode::Q    => Some(4),
-                KeyCode::W    => Some(5),
-                KeyCode::E    => Some(6),
-                KeyCode::A    => Some(7),
-                KeyCode::S    => Some(8),
-                KeyCode::D    => Some(9),
-                KeyCode::Z    => Some(10),
-                KeyCode::C    => Some(11),
-                KeyCode::Key4 => Some(12),
-                KeyCode::R    => Some(13),
-                KeyCode::F    => Some(14),
-                KeyCode::V    => Some(15),
-                _ => None
+            let mut ret = None;
+            
+            for i in 0..KEYS.len() {
+                if key == KEYS[i] {
+                    ret = Some(i as u8);
+                    break;
+                }
             }
+
+            ret
         } else {
             None
         };
