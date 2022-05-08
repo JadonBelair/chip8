bfbb4e9faa70af14723497b146095186842fdf9a
JadonBelair <46850171+JadonBelair@users.noreply.github.com>
2022-05-08 16:49:16 -0400

    added a speed slider

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,8 @@ async fn main() {
 
     let mut paused = false;
 
+    let mut speed = 5.;
+
     let mut chip8 = Chip8::new();
 
     // loads the rom with a single instruction to jump to itself
@@ -53,6 +55,7 @@ async fn main() {
     };
 
     let mut show_games = false;
+    let mut show_speed = false;
 
     chip8.load_rom(&rom);
 
@@ -60,20 +63,22 @@ async fn main() {
 
     loop {
 
-        // sets every key that is currently down
-        let mut keys = [false; 16];
-        for i in 0..KEYS.len() {
-            keys[i] = is_key_down(KEYS[i])
-        }
-        chip8.keyboard.set_keys(keys);
-        
-        // runs a single instruction from the ram if the game isn't paused
-        if !paused {
-            chip8.run_instruction();
+        for _i in 0..(speed as usize) {
+            // sets every key that is currently down
+            let mut keys = [false; 16];
+            for i in 0..KEYS.len() {
+                keys[i] = is_key_down(KEYS[i])
+            }
+            chip8.keyboard.set_keys(keys);
+            
+            // runs a single instruction from the ram if the game isn't paused
+            if !paused {
+                chip8.run_instruction();
 
-            if delta_time.elapsed() > (Duration::from_millis(17)) {
-                chip8.update_timers();
-                delta_time = Instant::now();
+                if delta_time.elapsed() > (Duration::from_millis(17)) {
+                    chip8.update_timers();
+                    delta_time = Instant::now();
+                }
             }
         }
 
@@ -87,7 +92,10 @@ async fn main() {
             for x in 0..64 {
                 // only draws white pixels
                 if display[y][x] != 0 {
-                    draw_rectangle((x as i32 * CELL_SIZE) as f32, (y as i32 * CELL_SIZE) as f32 + Y_OFFSET, CELL_SIZE as f32, CELL_SIZE as f32, WHITE);
+                    draw_rectangle((x as i32 * CELL_SIZE) as f32,
+                        (y as i32 * CELL_SIZE) as f32 + Y_OFFSET,
+                        CELL_SIZE as f32, CELL_SIZE as f32,
+                        WHITE);
                 }
             }
         }
@@ -96,11 +104,12 @@ async fn main() {
         draw_rectangle(0., 0., screen_width(), Y_OFFSET, GRAY);
 
         // creates the UI buttons
+        let roms_button = widgets::Button::new("Load Rom").position(vec2(5., 5.)).size(vec2(80., 20.));
         let wrap_button = widgets::Button::new(format!("{}able Screen Wrapping", if chip8.display.wrap {"Dis"} else {"En"}))
                         .position(vec2(90.,  5.)).size(vec2(200., 20.));
-        let roms_button = widgets::Button::new("Load Rom").position(vec2(5., 5.)).size(vec2(80., 20.));
         let pause_button = widgets::Button::new(format!("{}ause The Game", if paused {"Unp"} else {"P"}))
                         .position(vec2(295., 5.)).size(vec2(125., 20.));
+        let speed_button = widgets::Button::new("Speed").position(vec2(425., 5.)).size(vec2(80., 20.));
         
         // checks if the user wants screen wrapping enabled
         if wrap_button.ui(&mut *root_ui()) {
@@ -116,6 +125,10 @@ async fn main() {
         // pauses / unpauses the game
         if pause_button.ui(&mut *root_ui()) {
             paused = !paused;
+        }
+
+        if speed_button.ui(&mut *root_ui()) {
+            show_speed = !show_speed;
         }
 
         // draw a list of all available roms if the user requested it
@@ -147,6 +160,16 @@ async fn main() {
                 }
             });
         }
+
+        if show_speed {
+            let window = widgets::Window::new(2, vec2(425., 25.), vec2(600., 60.)).movable(false);
+
+            window.ui(&mut *root_ui(), |ui| {
+                ui.slider(3, "Speed", (1.)..(20.), &mut speed);
+            });
+        }
+
+        speed = speed.round();
         
         // will only start playing the audio if it isnt alreadly playing
         if !playing && chip8.st > 0 && !paused {
